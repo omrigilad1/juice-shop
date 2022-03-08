fd163185a35109b271c077cf84b7f3a5dabf8095
DaniellaSnyk <100514370+DaniellaSnyk@users.noreply.github.com>
2022-03-08 10:42:17 +0200

    Update protractor.subfolder.conf.js

--- protractor.subfolder.conf.js
+++ protractor.subfolder.conf.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2014-2021 Bjoern Kimminich.
  * SPDX-License-Identifier: MIT
  */
-
+const a = 1
 const protractorConfig = require('./protractor.conf.js').config
 
 protractorConfig.baseUrl = 'http://localhost:3001/subfolder'
